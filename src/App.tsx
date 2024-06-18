38ba5b44e10fe871ce0364d50f9734182d40f387
kimyonghui <kyhui1115@gmail.com>
2024-06-18 13:54:37 +0900

    chore: react-router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './layout/Main';
+import Introduce from './components/introduce/Introduce';
+import Skill from './components/skill/Skill';
+import Projects from './components/projects/Projects';
+import Background from './components/background/Background';
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <div className="w-screen h-screen">
+        <Routes>
+          <Route element={<Main />}>
+            <Route path="/introduce" element={<Introduce />} />
+            <Route path="/skill" element={<Skill />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/background" element={<Background />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
