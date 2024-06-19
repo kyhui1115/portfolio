8571a97986a5dadbf62cd50bc69fd822a50e3093
kimyonghui <kyhui1115@gmail.com>
2024-06-19 09:09:06 +0900

    design: 개인정보 관련 컴포넌트 UI 구현

---
+++ src/components/nav/UserInfoItem.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react';
+
+interface propTypes {
+  icon: ReactElement;
+  text: string;
+}
+
+export default function UserInfoItem({ icon, text }: propTypes) {
+  return (
+    <div className="flex items-center mt-1">
+      {icon}
+      <span className="ml-2 text-sm">{text}</span>
+    </div>
+  );
+}
