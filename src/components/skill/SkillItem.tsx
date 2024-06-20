0cc0f8e7f76f2c23458cf094d60df04a21faf980
kimyonghui <kyhui1115@gmail.com>
2024-06-20 09:12:45 +0900

    design: skill 페이지 UI 구현 및 Math 매서드를 통한 UI 로직 구현

---
+++ src/components/skill/SkillItem.tsx
@@ -0,0 +1,57 @@
+import { ReactElement, useRef } from 'react';
+import Item from './Item';
+import Line from './Line';
+
+interface propTypes {
+  name: string;
+  coor: {
+    top: number;
+    left: number;
+  };
+  items: item[];
+  monitorCenterCoor: {
+    top: number;
+    left: number;
+  };
+}
+
+interface item {
+  name: string;
+  icon: ReactElement;
+}
+
+export default function SkillItem({
+  name,
+  items,
+  coor,
+  monitorCenterCoor,
+}: propTypes) {
+  const skillRef = useRef<HTMLDivElement>(null);
+
+  const skillCoor = {
+    top: monitorCenterCoor.top + coor.top,
+    left: monitorCenterCoor.left + coor.left,
+  };
+
+  return (
+    <>
+      <div
+        className="absolute z-10 flex flex-col items-center w-48 bg-gray-500 rounded-xl"
+        style={{ top: skillCoor.top + 'px', left: skillCoor.left + 'px' }}
+        ref={skillRef}
+      >
+        <span className="mt-2 mb-4 text-2xl font-bold text-beige-100">
+          {name}
+        </span>
+        {items.map((item, i) => (
+          <Item key={i} name={item.name} icon={item.icon} />
+        ))}
+      </div>
+      <Line
+        skillCoor={skillCoor}
+        monitorCenterCoor={monitorCenterCoor}
+        skillRef={skillRef}
+      />
+    </>
+  );
+}
--- src/pages/Skill.tsx
+++ src/pages/Skill.tsx
@@ -1,11 +1,33 @@
 import Title from '../components/common/Title';
 import monitor from '../assets/monitor.svg';
+import SkillList from '../components/skill/SkillList';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Skill() {
+  const [monitorCenterCoor, setMonitorCenterCoor] = useState({
+    top: 0,
+    left: 0,
+  });
+  const monitorRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (monitorRef.current !== null) {
+      setMonitorCenterCoor({
+        top:
+          monitorRef.current?.offsetTop -
+          16 +
+          monitorRef.current?.offsetHeight / 2,
+        left:
+          monitorRef.current?.offsetLeft + monitorRef.current?.offsetWidth / 2,
+      });
+    }
+  }, []);
+
   return (
     <div className="relative flex items-center justify-center w-full h-full bg-beige-300">
       <Title text="skill" />
-      <img src={monitor} alt="monitor" className="w-40 h-40" />
+      <img src={monitor} alt="monitor" className="w-40 h-40" ref={monitorRef} />
+      <SkillList monitorCenterCoor={monitorCenterCoor} />
     </div>
   );
 }
