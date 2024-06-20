629177858402b500c9f9bc8bfdd5566fc0153e30
kimyonghui <kyhui1115@gmail.com>
2024-06-21 00:02:44 +0900

    design: Title 공통 컴포넌트 컬러 prop 추가

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -1,10 +1,15 @@
 interface propTypes {
   text: string;
+  color?: string;
 }
 
-export default function Title({ text }: propTypes) {
+export default function Title({ text, color }: propTypes) {
   return (
-    <span className="absolute text-2xl font-semibold text-gray-400 right-4 top-2">
+    <span
+      className={`absolute text-2xl font-semibold right-4 top-2 ${
+        color === undefined ? 'text-gray-400' : color
+      }`}
+    >
       {text}
     </span>
   );
