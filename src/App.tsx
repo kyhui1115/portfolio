680b5767b0ed09e99881bf279573afa5ed98b1f2
kimyonghui <kyhui1115@gmail.com>
2024-06-27 15:33:15 +0900

    feat: layout 및 프로젝트 상세 페이지 router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import AboutMe from './pages/AboutMe';
-import Skill from './pages/Skill';
-import Projects from './pages/Projects';
-import Background from './pages/Background';
-import Nav from './components/nav/Nav';
-import ScrollText from './components/AboutMe/ScrollText';
-import { useState } from 'react';
-import Footer from './components/Footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProjectDetail from './pages/ProjectDetail';
+import Main from './layout/Main';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState(0);
-
   return (
-    <div className="relative flex flex-col w-full">
-      <Nav currentPage={currentPage} />
-      <AboutMe setCurrentPage={setCurrentPage} />
-      <Skill setCurrentPage={setCurrentPage} />
-      <Projects setCurrentPage={setCurrentPage} />
-      <Background setCurrentPage={setCurrentPage} />
-      <Footer />
-      {currentPage < 3 ? <ScrollText /> : null}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/project/:id" element={<ProjectDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/layout/Main.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Nav from '../components/nav/Nav';
+import AboutMe from '../pages/AboutMe';
+import Skill from '../pages/Skill';
+import Projects from '../pages/Projects';
+import Background from '../pages/Background';
+import Footer from '../components/Footer/Footer';
+import ScrollText from '../components/AboutMe/ScrollText';
+
+export default function Main() {
+  const [currentPage, setCurrentPage] = useState(0);
+  return (
+    <div className="relative flex flex-col w-full">
+      <Nav currentPage={currentPage} />
+      <AboutMe setCurrentPage={setCurrentPage} />
+      <Skill setCurrentPage={setCurrentPage} />
+      <Projects setCurrentPage={setCurrentPage} />
+      <Background setCurrentPage={setCurrentPage} />
+      <Footer />
+      {currentPage < 3 ? <ScrollText /> : null}
+    </div>
+  );
+}
